55f196a71954bcb7e3ad7f0d14659a4a921db189
david.fischak@eodc.eu <david.fischak@eodc.eu>
2025-03-19 14:05:07 +0100

    Add geo packages to Julia

---
+++ kernel-julia/container/julia-packages.jl
@@ -0,0 +1,11 @@
+using Pkg
+
+
+Pkg.add(Pkg.PackageSpec(name="NCDatasets",  version="0.14.6"))
+Pkg.add(Pkg.PackageSpec(name="Geodesy",     version="1.1.0"))
+Pkg.add(Pkg.PackageSpec(name="GDAL",        version="1.10.0"))
+Pkg.add(Pkg.PackageSpec(name="LibGEOS",     version="0.9.4"))
+Pkg.add(Pkg.PackageSpec(name="GeoJSON",     version="0.8.2"))
+Pkg.add(Pkg.PackageSpec(name="Leaflet",     version="0.1.1"))
+Pkg.add(Pkg.PackageSpec(name="TileProviders",   version="0.1.4"))
+Pkg.add(Pkg.PackageSpec(name="ZarrDatasets",    version="0.1.3"))
