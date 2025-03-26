40646e8e843c5c52ddb7833aa3002ccefe8e47a0
david.fischak@eodc.eu <david.fischak@eodc.eu>
2025-03-26 13:17:29 +0100

    Add some more packages

--- kernel-julia/container/julia-packages.jl
+++ kernel-julia/container/julia-packages.jl
@@ -11,7 +11,12 @@ packages = [
         ("Blink",       "0.12.9"), # requires webio-jupyter-extension
         ("GADM",        "1.2.0"),
     ("TileProviders",   "0.1.4"),
-    ("ZarrDatasets",    "0.1.3")
+    ("ZarrDatasets",    "0.1.3"),
+    ("CSV",             "0.10.15"),
+    ("DataFrames",      "1.7.0"),
+    ("Plots",           "1.40.11"),
+    ("PyCall",          "1.96.4"),
+    #(<package name>, <version>)
 ]
 
 for (name, version) in packages
